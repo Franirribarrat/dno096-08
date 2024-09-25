32a6d1228c1085e9e18ce4f8a366fe7691cb7104
Franirribarrat <franirribarrat@uc.cl>
2024-09-25 12:18:03 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,72 @@
+[
+    {
+        "nombre": "Libro Aves Lafkenche",
+        "fecha": "2018",
+        "signo": "Editorial",
+        "foto": "https://github.com/Franirribarrat/dno096-08/blob/main/fotos_Mesa%20de%20trabajo%201%20copia.png?raw=true",
+        "github": "https://github.com/franirribarrat"
+    },
+    {
+        "nombre": "Tulilamp",
+        "fecha": "2022",
+        "signo": "Industrial",
+        "foto": "https://github.com/Franirribarrat/dno096-08/blob/main/fotos_Mesa%20de%20trabajo%201%20copia%209.png?raw=true",
+        "github": "https://github.com/franirribarrat"
+    },
+    {
+        "nombre": "Revista Voces de la Araucanía",
+        "fecha": "2000-04-20",
+        "signo": "Editorial",
+        "foto": "https://raw.githubusercontent.com/Franirribarrat/dno096-08/refs/heads/main/fotos_Mesa%20de%20trabajo%201%20copia%208.png",
+        "github": "https://github.com/franirribarrat"
+    },
+    {
+        "nombre": "Afiche Barrio Yungay",
+        "fecha": "2001-04-10",
+        "signo": "Editorial",
+        "foto": "https://github.com/Franirribarrat/dno096-08/blob/main/fotos_Mesa%20de%20trabajo%201%20copia%207.png?raw=true",
+        "github": "https://github.com/franirribarrat"
+    },
+    {
+        "nombre": "Macro lilas",
+        "fecha": "2001-09-21",
+        "signo": "Fotografia",
+        "foto": "https://raw.githubusercontent.com/Franirribarrat/dno096-08/refs/heads/main/fotos_Mesa%20de%20trabajo%201.png",
+        "github": "https://github.com/franirribarrat"
+    },
+    {
+        "nombre": "Flamencos",
+        "fecha": "2001-08-10",
+        "signo": "Fotografia",
+        "foto": "https://github.com/Franirribarrat/dno096-08/blob/main/fotos_Mesa%20de%20trabajo%201%20copia%206.png?raw=true",
+        "github": "https://github.com/franirribarrat"
+    },
+    {
+        "nombre": "Banca",
+        "fecha": "2023",
+        "signo": "Industrial",
+        "foto": "https://raw.githubusercontent.com/Franirribarrat/dno096-08/refs/heads/main/fotos_Mesa%20de%20trabajo%201%20copia%205.png",
+        "github": "https://github.com/franirribarrat"
+    },
+    {
+        "nombre": "Constanza Lagos",
+        "fecha": "1998-09-08",
+        "signo": "virgo",
+        "foto": "https://github.com/Franirribarrat/dno096-08/blob/main/fotos_Mesa%20de%20trabajo%201%20copia%204.png?raw=true",
+        "github": "https://github.com/franirribarrat"
+    },
+    {
+        "nombre": "Fernanda Mamani",
+        "fecha": "2010-09-19",
+        "signo": "Fotografia",
+        "foto": "https://github.com/Franirribarrat/dno096-08/blob/main/fotos_Mesa%20de%20trabajo%201%20copia%203.png?raw=true",
+        "github": "https://github.com/franirribarrat"
+    },
+    {
+        "nombre": "Kimberly McCartney",
+        "fecha": "2001-02-16",
+        "signo": "Fotografia",
+        "foto": "https://github.com/Franirribarrat/dno096-08/blob/main/fotos_Mesa%20de%20trabajo%201%20copia%202.png?raw=true",
+        "github": "https://github.com/franirribarrat"
+    }
+]
